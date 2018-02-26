944278443a8bd971a80d21d9d86b3d08a60b267f
PHPaki <706947479@qq.com>
2018-02-26 09:16:45 +0800

    增加评论的权限判断

--- application/home/controller/Comment.php
+++ application/home/controller/Comment.php
@@ -9,14 +9,17 @@ class Comment extends Base
     //评论添加后台验证,返回新增数量
     public function commentAddExecu()
     {
+        if (is_null(USER_ID)) {
+            return 0;
+        }
         $data['content'] = $this->request->param('content');
         $check = $this->validate($data, 'Comment');
         if ($check !== true) {
-            $this->error($check);
+            return $check;
         }
         $data['user_id'] = $this->request->session('user_id');
         $article = Article::get($this->request->param('article_id'));
         $res = $article->comment()->save($data);
-        return $res;
+        return true;
     }
 }
